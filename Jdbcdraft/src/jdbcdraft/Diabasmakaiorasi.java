93a413e80cac44666dd82bed7de38bd529ded173
lefteris08 <33422312+lefteris08@users.noreply.github.com>
2017-11-30 15:01:51 +0200

    oli i allagi

--- Jdbcdraft/src/jdbcdraft/Diabasmakaiorasi.java
+++ Jdbcdraft/src/jdbcdraft/Diabasmakaiorasi.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -25,37 +26,39 @@ public class Diabasmakaiorasi {
     static final String DB_URL = "jdbc:mysql://localhost:3306/catalog";
     static final String USER = "root";
     static final String PASS = "admin";
+    static Connection conn = null;
+    static Statement stmt = null;
+    static PreparedStatement pstmt = null;
+    static ArrayList<bdaystoixeia> bdays = new ArrayList<bdaystoixeia>();
+
+    public void sind() {
+
+        System.out.println("Connecting to database...");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+//           
+        }
+        try {
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (SQLException ex) {
+            //Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        if (conn == null) {
+
+            System.out.println("wtf");
+        }
+        // stmt = conn.createStatement();
+
+// System.out.println("den paizei");
+    }
+//emfanizw tous xristes
 
     public void Diabasmakaiorasi() {
+        sind();
         try {
-            // TODO code application logic here
-            Connection conn = null;
-            Statement stmt = null;
-
-            System.out.println("Connecting to database...");
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                // Class.forName("com.mysql.jdbc.Driver");
-            } catch (ClassNotFoundException ex) {
-//            Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            try {
-                conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            } catch (SQLException ex) {
-                //Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-            try {
-                if (conn == null) {
-
-                    System.out.println("wtf");
-                }
-                stmt = conn.createStatement();
-            } catch (SQLException ex) {
-                // Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
-            }
-// System.out.println("den paizei");
-
+            stmt = conn.createStatement();
             System.out.println("Creating statement...");
             String sql;
             sql = "SELECT * From members";
@@ -72,7 +75,7 @@ public class Diabasmakaiorasi {
                 }
 
                 rs.close();
-
+//slect count (*)from members
             } catch (SQLException ex) {
                 //   Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -80,125 +83,65 @@ public class Diabasmakaiorasi {
 
             stmt.close();
             conn.close();
+
         } catch (SQLException ex) {
             Logger.getLogger(Diabasmakaiorasi.class.getName()).log(Level.SEVERE, null, ex);
-//            Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
+//bazw 5 times
 
     public void insertInt() {
+        sind();
+        try {
+            String sql = "insert into members(Fname,LastName,tel1,tel2) values(\"lef\",\"mal\",\"2106464643\",\"2106464644\"),(\"lefteris\",\"malindretos\",\"2106464643\",\"2106464644\"),(\"efl\",\"alm\",\"1064646432\",\"1064646442\"),(\"fle\",\"lma\",\"0646464321\",\"0646464421\"),(\"ioannis\",\"dretos\",\"6464643210\",\"6464644210\"),(\"oannisi\",\"retosd\",\"4646432106\",\"4646442106\");";
+//anti gia olli tin diadikasia \"\" mporousa apla na balw ''
+            pstmt = conn.prepareStatement(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(Diabasmakaiorasi.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
         try {
-            // TODO code application logic here
-            Connection conn = null;
-            PreparedStatement stmt = null;
+            pstmt.executeUpdate();
 
-            System.out.println("Connecting to database...");
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                // Class.forName("com.mysql.jdbc.Driver");
-            } catch (ClassNotFoundException ex) {
-//            Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            try {
-                conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            } catch (SQLException ex) {
-                //Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        } catch (SQLException ex) {
+            //   Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-            try {
-                if (conn == null) {
+        try {
+            pstmt.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Diabasmakaiorasi.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-                    System.out.println("wtf");
-                }
-                String sql;
-                sql = "insert into members(Fname,LastName,tel1,tel2) values(\"lef\",\"mal\",\"2106464643\",\"2106464644\"),(\"lefteris\",\"malindretos\",\"2106464643\",\"2106464644\"),(\"efl\",\"alm\",\"1064646432\",\"1064646442\"),(\"fle\",\"lma\",\"0646464321\",\"0646464421\"),(\"ioannis\",\"dretos\",\"6464643210\",\"6464644210\"),(\"oannisi\",\"retosd\",\"4646432106\",\"4646442106\");";
-//anti gia olli tin diadikasia \"\" mporousa apla na balw ''
-                stmt = conn.prepareStatement(sql);
-            } catch (SQLException ex) {
-                // Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
-            }
-// System.out.println("den paizei");
-
-            System.out.println("Creating statement...");
-            // ResultSet rs;
-            try {
-                stmt.executeUpdate();
-
-            } catch (SQLException ex) {
-                //   Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            System.out.println("egine");
-
-            stmt.close();
+        try {
             conn.close();
         } catch (SQLException ex) {
             Logger.getLogger(Diabasmakaiorasi.class.getName()).log(Level.SEVERE, null, ex);
-//            Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
+//na dw to record me basi to string
 
     public void showrec() {
         Scanner stdin = new Scanner(System.in);
         String name = "";
         String lname = "";
         System.out.println("What is your name?");
-        //String name = 
         name = stdin.nextLine();
-        //  System.out.println("Hello " + Klasi.getName());
-
         name = name.replaceAll("\\s+", "");
         System.out.println("What is your surname?");
         lname = stdin.nextLine();
-        System.out.println(lname);
         lname = lname.replaceAll("\\s+", "");
-        System.out.println(lname);
-
+        sind();
         try {
-            // TODO code application logic here
-            Connection conn = null;
-            Statement stmt = null;
-
-            System.out.println("Connecting to database...");
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                // Class.forName("com.mysql.jdbc.Driver");
-            } catch (ClassNotFoundException ex) {
-                System.out.println("den brethike i klasi");
-//            Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            try {
-                conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            } catch (SQLException ex) {
-
-                System.out.println("den egine sindesi");
-                //Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-            try {
-                if (conn == null) {
-
-                    System.out.println("wtf");
-                }
-
-                stmt = conn.createStatement();
-            } catch (SQLException ex) {
-                // Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
-            }
-// System.out.println("den paizei");
 
             System.out.println("Creating statement...");
             String sql;
-            PreparedStatement pstmt = conn.prepareStatement(
+            pstmt = conn.prepareStatement(
                     "Select* from members where Fname =? and LastName=?");
             pstmt.setString(1, name);
             pstmt.setString(2, lname);
-
-            //sql = "Select* from members where Fname ='" + name + "' and LastName=" + lname+"";
-            //System.out.println(sql);
-            //System.out.println(stmt);
             ResultSet rs;
             int k = 0;
             try {
@@ -220,27 +163,26 @@ public class Diabasmakaiorasi {
 
             } catch (SQLException ex) {
                 System.out.println("den ipirxe egrafi");
-                //   Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
             }
             System.out.println("egine");
 
             pstmt.close();
             conn.close();
+
         } catch (SQLException ex) {
-            Logger.getLogger(Diabasmakaiorasi.class.getName()).log(Level.SEVERE, null, ex);
-//            Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Diabasmakaiorasi.class
+                    .getName()).log(Level.SEVERE, null, ex);
         }
 
     }
+//alazw me basi to name
 
     public void changeN() {
         Scanner stdin = new Scanner(System.in);
         String number = "";
         String lname = "";
         System.out.println("What is your number?");
-        //String name = 
         number = stdin.nextLine();
-        //  System.out.println("Hello " + Klasi.getName());
 
         number = number.replaceAll("\\s+", "");
         System.out.println(number);
@@ -249,66 +191,137 @@ public class Diabasmakaiorasi {
         System.out.println(lname);
         lname = lname.replaceAll("\\s+", "");
         System.out.println(lname);
-
+        sind();
         try {
             // TODO code application logic here
-            Connection conn = null;
-            Statement stmt = null;
 
-            System.out.println("Connecting to database...");
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                // Class.forName("com.mysql.jdbc.Driver");
-            } catch (ClassNotFoundException ex) {
-                System.out.println("den brethike i klasi");
-//            Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            try {
-                conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            } catch (SQLException ex) {
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (SQLException ex) {
 
-                System.out.println("den egine sindesi");
-                //Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            System.out.println("den egine sindesi");
+        }
 
-            try {
-                if (conn == null) {
+        if (conn == null) {
 
-                    System.out.println("wtf");
-                }
+            System.out.println("wtf");
+        }
 
-                stmt = conn.createStatement();
-            } catch (SQLException ex) {
-                // Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
-            }
-// System.out.println("den paizei");
+        try {
+            stmt = conn.createStatement();
 
-            System.out.println("Creating statement...");
             String sql;
-            PreparedStatement pstmt = conn.prepareStatement(
-                    " update members set  tel1= ? where  members.LastName =?");
-            System.out.println(number);
-            System.out.println(lname);
-            pstmt.setString(1, number);
-            pstmt.setString(2, lname);
+            pstmt = conn.prepareStatement(" update members set  tel1= ? where  members.LastName =?");
 
+            System.out.println(number);
+
+            System.out.println(lname);
+
+            pstmt.setString(
+                    1, number);
+            pstmt.setString(
+                    2, lname);
             pstmt.execute();
 
-            //sql = "Select* from members where Fname =" + name + " and LastName=" + lname+"";
-            //System.out.println(sql);
-            //System.out.println(stmt);
             pstmt.close();
+
             conn.close();
         } catch (SQLException ex) {
             Logger.getLogger(Diabasmakaiorasi.class.getName()).log(Level.SEVERE, null, ex);
-//            Logger.getLogger(kodikas2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void dimpin() {
+        sind();
+        StringBuffer sb = new StringBuffer("");
+        sb.append("CREATE TABLE birthday (Bday VARCHAR(20) ,id int(100) unsigned not null , Primary key (id),FOREIGN KEY (`id`) REFERENCES `members` (`id`));");
+        Scanner stdin = new Scanner(System.in);
+        System.out.println("What the  name ");
+        String name = stdin.nextLine();
+        name = name.replaceAll("\\s+", "");
+
+        try {
+            pstmt = conn.prepareStatement(sb.toString());
+
+            // pstmt.setString(1, name);
+            pstmt.execute();
+            pstmt.close();
+            conn.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Diabasmakaiorasi.class
+                    .getName()).log(Level.SEVERE, null, ex);
         }
 
     }
 
-//String surname = 
-    //Klasi.setsurname(stdin.nextLine());
-    //System.out.println("What is your Fathers name?");
-    //String Fname =
-    //Klasi.setFname(stdin.nextLine());
+    public void eisBirthday() {
+        Scanner stdin = new Scanner(System.in);
+
+        sind();
+        StringBuffer sb = new StringBuffer("");
+        sb.append("select  Fname,LastName,id  from catalog.members;");
+        try {
+            pstmt = conn.prepareStatement(sb.toString());
+            int k = 0;
+            ResultSet rs = pstmt.executeQuery();
+            String fname = "";
+            String lname = "";
+            String bday = "";
+            //bday = stdin.nextLine();
+//System.out.println(rs);
+            int ida = 0;
+            while (rs.next()) {
+                k++;
+                bdaystoixeia node = new bdaystoixeia();
+                fname = rs.getString(1);
+                ida = rs.getInt(3);
+                lname = rs.getString(2);
+                System.out.println("dwse mou ta stoixeia gia ton " + fname + " " + lname);
+                bday = stdin.nextLine();
+                bday = bday.replaceAll("\\s+", "");
+                node.bday = bday;
+                node.id = ida;//edw issos thelei na kanei ena elexno me mia sinartisi connect kai elennxos
+                bdays.add(node);
+            }
+
+            if (k == 0) {
+                System.out.println("den ipirxe o xristis");
+            }
+            rs.close();
+
+            pstmt.close();
+//            conn.close();
+
+            StringBuffer sb2 = new StringBuffer("");
+            sb2.append("INSERT INTO  birthday (Bday  ,id) VALUES(?,?);");
+            pstmt = conn.prepareStatement(sb2.toString());
+
+            //System.out.println(lname);
+            for (bdaystoixeia string : bdays) {
+System.out.println(sb2.toString());
+               
+                pstmt.setString(1,string.bday);
+                System.out.println("exw   "+string.bday+"     kai     " +Integer.valueOf(string.id));
+                pstmt.setInt(2,string.id);
+            
+            pstmt.execute();
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Diabasmakaiorasi.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            pstmt.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Diabasmakaiorasi.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            conn.close();
+            // pstmt.setString(1, name);
+        } catch (SQLException ex) {
+            Logger.getLogger(Diabasmakaiorasi.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
--- Jdbcdraft/src/jdbcdraft/Jdbcdraft.java
+++ Jdbcdraft/src/jdbcdraft/Jdbcdraft.java
@@ -5,6 +5,8 @@
  */
 package jdbcdraft;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lefte
@@ -15,27 +17,30 @@ public class Jdbcdraft {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Diabasmakaiorasi kl=new Diabasmakaiorasi();
-       
-        System.out.println("+++++++++++++++");
- // kl.Diabasmakaiorasi();
-  
-        System.out.println("+++++++++++++++");
-  kl.insertInt();
-  kl.Diabasmakaiorasi();
-int o=0;
-  while (o<1){
-  kl.showrec();
-  o++;
-  }  
-        System.out.println("77777777777777777777777777777777777777777777777777777777777777777");
-  kl.changeN();
-  
-        System.out.println("77777777777777777777777777777777777777777777777777777777777777777");
-  kl.Diabasmakaiorasi();
-// TODO code application logic here
-        // TODO code application logic here
+        Scanner stdin = new Scanner(System.in);
+        String epilogi = "";
+        int ep;
+        String lname = "";
+        System.out.println("epelexe:\n 0 gia na deis oles tis times\n 1 gia na baleis 5 times \n 2 gia na alaxeis me basi to last name \n 3 na deos tis times me basi  fname lname\n 4 gia na deis posa record exeis \n 5 na dimiourgiseis ena pinaka \n 6 na baleis ta stoixeia ston birthdays meso tou member \n 7 des pioi anthropoi exoun tin idia imerominia ");
+        epilogi = stdin.nextLine();
+        ep = Integer.valueOf(epilogi);
+
+        Diabasmakaiorasi kl = new Diabasmakaiorasi();
+
+        if (ep == 0) {
+            kl.Diabasmakaiorasi();
+        }
+        if (ep == 1) {
+            kl.insertInt();
+        } else if (ep == 2) {
+            kl.changeN();
+        } else if (ep == 3) {
+            kl.showrec();
+        } else if (ep == 4) {
+        } else if (ep == 5) {
+            kl.dimpin();
+        } else if (ep == 6) {
+            kl.eisBirthday();
+        }
     }
-    
 }
